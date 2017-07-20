b95c257b84bc96266a22e05bb43242286128115a
nghiadk <nghiadk1102@gmail.com>
2017-07-20 16:38:29 +0700

    comment views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,5 +4,6 @@ class StaticPagesController < ApplicationController
     return unless user_signed_in?
     @post  = current_user.posts.build
     @feed_items = current_user.feed.order_by_desc.page(params[:page]).per Settings.controller.per_page
+    @comment = Comment.new
   end
 end
