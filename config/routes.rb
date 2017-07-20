b11fc96640a45a16227d1c832775b0a17bdded5e
nghiadk <nghiadk1102@gmail.com>
2017-07-20 16:42:12 +0700

    comment with controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,15 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "/users/sign_out" => "devise/sessions#destroy"
   end
+
   root "static_pages#home"
   get "/help", to: "static_pages#help"
   resources :posts, only: [:create, :destroy]
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :comments
+  resources :relationships, only: [:create, :destroy]
 end
